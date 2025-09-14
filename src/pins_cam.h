14127e4f7e4fe1955973ab6a063f574b7da3f5ff
Costin STROIE <costinstroie@eridu.eu.org>
2025-09-14 17:26:49 +0300

    fix: update pin definitions for ESP32-CAM board compatibility

--- src/pins_cam.h
+++ src/pins_cam.h
@@ -21,20 +21,20 @@
 
 // Pin definitions for ESP32-CAM
 // Note: Some pins are reserved for camera interface
-#define DEFAULT_I2S_DOUT         26  ///< I2S Data Out pin
-#define DEFAULT_I2S_BCLK         27  ///< I2S Bit Clock pin
-#define DEFAULT_I2S_LRC          25  ///< I2S Left/Right Clock pin
-#define DEFAULT_LED_PIN           2  ///< ESP32 internal LED pin
-#define DEFAULT_ROTARY_CLK       18  ///< Rotary encoder clock pin
-#define DEFAULT_ROTARY_DT        19  ///< Rotary encoder data pin
-#define DEFAULT_ROTARY_SW        23  ///< Rotary encoder switch pin
+#define DEFAULT_I2S_DOUT         15  ///< I2S Data Out pin
+#define DEFAULT_I2S_BCLK          4  ///< I2S Bit Clock pin
+#define DEFAULT_I2S_LRC           2  ///< I2S Left/Right Clock pin
+#define DEFAULT_LED_PIN          -1  ///< ESP32 internal LED pin
+#define DEFAULT_ROTARY_CLK       -1  ///< Rotary encoder clock pin
+#define DEFAULT_ROTARY_DT        -1  ///< Rotary encoder data pin
+#define DEFAULT_ROTARY_SW        -1  ///< Rotary encoder switch pin
 #define DEFAULT_TOUCH_PLAY       12  ///< Touch button play/pause pin
 #define DEFAULT_TOUCH_NEXT       13  ///< Touch button next/volume-up pin
 #define DEFAULT_TOUCH_PREV       14  ///< Touch button previous/volume-down pin
 #define DEFAULT_TOUCH_THRESHOLD  40  ///< Touch threshold value
 #define DEFAULT_TOUCH_DEBOUNCE   50  ///< Touch debounce time in milliseconds
-#define DEFAULT_BOARD_BUTTON      0  ///< ESP32 board button pin (with internal pull-up resistor)
-#define DEFAULT_DISPLAY_SDA      21  ///< OLED display SDA pin
+#define DEFAULT_BOARD_BUTTON     -1  ///< ESP32 board button pin (with internal pull-up resistor)
+#define DEFAULT_DISPLAY_SDA       0  ///< OLED display SDA pin
 #define DEFAULT_DISPLAY_SCL      22  ///< OLED display SCL pin
 #define DEFAULT_DISPLAY_TYPE      0  ///< OLED display type (index)
 #define DEFAULT_DISPLAY_ADDR   0x3C  ///< OLED display I2C address
